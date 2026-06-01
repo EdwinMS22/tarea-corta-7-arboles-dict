a1cc018e4515fc2914ce14a0047a1fe216942b43
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-01 04:28:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Tarea Corta 7 - Árboles de búsqueda binaria y diccionarios
+Realizado por: Edwin Muñoz Sandí 2025075200
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// ##########################################################
+// Tarea Corta 7 - Estructuras de Datos
+// Edwin Andrey Muñoz Sandí 2025075200
+// ############################################################
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+}
