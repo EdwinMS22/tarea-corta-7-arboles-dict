bbb17953779019cce8b55ba521af131fabbc79dc
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-03 02:22:14 -0600

    Add BSTDictionary attributes, constructor and destructor

--- BSTDictionary.h
+++ BSTDictionary.h
@@ -6,5 +6,14 @@
 
 template <typename K, typename V>
 class BSTDictionary : public Dictionary<K, V> {
+private:
+	BSTree<Pair<K, V>>* tree;
 
+public:
+	BSTDictionary() {
+		tree = new BSTree<Pair<K, V>>();
+	}
+	~BSTDictionary() {
+		delete tree;
+	}
 };
