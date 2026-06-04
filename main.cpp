6fe09de0c173ebd4af8db14a12f3ce30d6ed707a
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-03 21:33:18 -0600

    Implement printDict to print dictionaries in main

--- main.cpp
+++ main.cpp
@@ -10,10 +10,27 @@
 using std::cout;
 using std::string;
 
+static void printDict(Dictionary<int, string>* dict) {
+    cout << "{ ";
+    dict->print();
+    cout << "}\n";
+}
+
 int main() {
     Dictionary<int, string>* dict1 = new BSTDictionary<int, string>();
     Dictionary<int, string>* dict2 = new BSTDictionary<int, string>();
 
+    int option = -1;
+    while (option != 0) {
+        cout << "\tDictionaries\n";
+        cout << "Dictionary 1:\n";
+        printDict(dict1);
+        cout << "Dictionary 2:\n";
+        printDict(dict2);
+        option = 0;
+    }
+
     delete dict1;
     delete dict2;
+    return 0;
 }
